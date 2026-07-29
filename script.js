89c4ab7c82a6c366ad3373290d4fc9ce488eed4f
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2026-07-30 01:38:08 +0200

    request loan

--- script.js
+++ script.js
@@ -160,6 +160,24 @@ btnTransfer.addEventListener('click', function(e){
 
 });
 
+btnLoan.addEventListener('click', function(e){
+  
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some(move => mov >= amount * 0.1)){
+    
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+
+  }
+
+  inputLoanAmount.value = '';
+
+});
+
 btnClose.addEventListener('click', function(e){
   
   e.preventDefault();
